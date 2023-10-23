f3681c595a59786c0b85d22bac3dc6c06355365f
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-10-23 18:01:27 +0000

    feat: improved meta description

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;700&family=Inknut+Antiqua:wght@300;700&family=La+Belle+Aurore&display=swap" rel="stylesheet" />
 
-        <meta name="description" content="Learn how to invest, buy, sell or rent in the best possible way thanks to Katherinrios." />
+        <meta name="description" content="Learn how to invest, buy, sell or rent in the best possible way in the real estate sector thanks to Katherinrios." />
       </Head>
       <body>
         <Main />
