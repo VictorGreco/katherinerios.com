2381235e763d94045f42a24e3a7e9078d56d9494
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-10-23 17:37:24 +0000

    feat: added contrast for accessibility

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Katherinrios | Real estate</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
         <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap" rel="stylesheet" />
