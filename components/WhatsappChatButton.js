9ed2791b3266b26bbf6e3c143212fdfab18cbe71
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-11-12 11:55:03 +0000

    removed unnecessary code

--- components/WhatsappChatButton.js
+++ components/WhatsappChatButton.js
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Backdrop from '@mui/material/Backdrop';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
 
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 import styles from '../styles/WhatsappChatButton.module.css';
 
 
 export default function WhatsappChatButton() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
     <Box className={styles.whatsappWrapper}>
